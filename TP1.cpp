1e5f2a5445efe6f6160acef41d4341f731c44bce
Florian RIBOU <florian.ribou@gmail.com>
2019-01-26 18:24:06 +0100

    initial commit

---
+++ TP1.cpp
@@ -0,0 +1,28 @@
+# include <iostream>
+
+using namespace std;
+
+// renvoie un tableau avec la liste des sommets constituants une chaine augmentante
+int* chaineaugmentante(int *ch, int **c, int **f, int s, int t)
+{
+
+  return ch;
+}
+
+int main()
+{
+  // matrice des capacités
+  int **c;
+  // matrice des flots
+  int **f;
+  // sommet source
+  int s;
+  // sommet destination
+  int t;
+  // tableau d'éléments
+  int *ch = new int[t+1];
+
+  chaineaugmentante(ch,c,f,s,t);
+  cout << "Fin du main" << endl ;
+  return 0;
+}
